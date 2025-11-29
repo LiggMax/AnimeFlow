f29ea54d504ad48321427fc7535755763f738f1f
LiggMax <wenzhouli06@gmail.com>
2025-11-30 05:43:24 +0800

    feat(anime detail): 初步实现粘性吸顶

--- lib/pages/animedetail/index.dart
+++ lib/pages/animedetail/index.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:anime_flow/controllers/theme_controller.dart';
 import 'package:anime_flow/models/hot_item.dart';
-import 'package:flutter/services.dart';
 
 import 'head.dart';
 
@@ -29,18 +27,68 @@ class _AnimeDetailPageState extends State<AnimeDetailPage> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: SingleChildScrollView(
-        child: Column(
-          children: [
-            HeadView(data: animeData),
-            SizedBox(height: 10),
-            Column(
-              children: List<Widget>.generate(105, (index) => Text('$index')),
+    List<Tab> tabList = [Tab(text: '简介'), Tab(text: '评论'), Tab(text: '相关')];
+
+    return DefaultTabController(
+      length: tabList.length,
+      child: Scaffold(
+        body: CustomScrollView(
+          slivers: [
+            SliverAppBar(
+              pinned: true,
+              expandedHeight: 300,
+              flexibleSpace: FlexibleSpaceBar(
+                background: Image.network(
+                  animeData.images.large,
+                  fit: BoxFit.cover,
+                ),
+              ),
+            ),
+            SliverPersistentHeader(
+              delegate: _StickyTab(tabList: tabList),
+              pinned: true, // 添加这行使Tab吸顶
+            ),
+            SliverFillRemaining(
+              child: TabBarView(
+                children: [
+                  Center(child: Text('简介')),
+                  Center(child: Text('评论')),
+                  Center(child: Text('相关')),
+                ],
+              ),
             ),
           ],
         ),
       ),
     );
+  }
+}
+
+class _StickyTab extends SliverPersistentHeaderDelegate {
+  final List<Tab> tabList;
+
+  _StickyTab({required this.tabList});
+
+  @override
+  Widget build(
+    BuildContext context,
+    double shrinkOffset,
+    bool overlapsContent,
+  ) {
+    return Container(
+      color: Colors.white,
+      child: TabBar(tabs: tabList),
+    );
+  }
+
+  @override
+  double get maxExtent => 30;
+
+  @override
+  double get minExtent => 30;
+
+  @override
+  bool shouldRebuild(covariant SliverPersistentHeaderDelegate oldDelegate) {
+    return false;
   }
 }
