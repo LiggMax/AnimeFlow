cc5ee0a6c15acdfaf6e3255fc1f79dfb58b60647
LiggMax <wenzhouli06@gmail.com>
2025-12-02 16:22:40 +0800

    feat(search): 实现基于XPath的搜索结果解析

--- lib/controllers/video/video_resources_controller.dart
+++ lib/controllers/video/video_resources_controller.dart
@@ -1,3 +1,33 @@
-class VideoResourcesController {
+import 'package:anime_flow/utils/getConfigFlie.dart';
+import 'package:html/parser.dart';
+import 'package:logger/logger.dart';
+import 'package:xpath_selector_html_parser/xpath_selector_html_parser.dart';
 
+class VideoResourcesController {
+  static Logger logger = Logger();
+  //解析html搜索页
+  static void parseSearchHtml(String searchHtml) async {
+    final config = await GetConfigFile.loadPluginConfig();
+    final String searchList = config['searchList'];
+    final String searchName = config['searchName'];
+    final String searchLink = config['searchLink'];
+
+    final parser = parse(searchHtml).documentElement!;
+
+    final searchListElement = parser.queryXPath(searchList);
+    final searchNameListElement = parser.queryXPath('$searchList$searchName');
+    final searchLinkListElement = parser.queryXPath('$searchList$searchLink');
+
+    for(var element in searchListElement.nodes) {
+      logger.i("搜索结果列表: ${element.text}");
+    }
+
+    for(var element in searchNameListElement.nodes) {
+      logger.i("搜索结果名称: ${element.text}");
+    }
+
+    for(var element in searchLinkListElement.nodes) {
+      logger.i("搜索结果链接: ${element.attributes['href']}");
+    }
+  }
 }
--- lib/http/requests/web_request.dart
+++ lib/http/requests/web_request.dart
@@ -1,5 +1,6 @@
 import 'dart:math';
 
+import 'package:anime_flow/controllers/video/video_resources_controller.dart';
 import 'package:anime_flow/http/api/common_api.dart';
 import 'package:anime_flow/utils/getConfigFlie.dart';
 import 'package:anime_flow/utils/http/dio_request.dart';
@@ -14,17 +15,16 @@ class WebRequest {
   static Future<String> getSearchSubjectListService(String keyword) async {
     final config = await GetConfigFile.loadPluginConfig();
 
-    final String baseURL = config['baseURL'];
     final String searchURL = config['searchURL'];
     final userAgent = userAgentsList[Random().nextInt(userAgentsList.length)];
 
-    final response = await dioRequest
-        .get(searchURL.replaceFirst("{keyword}", keyword), options: Options(
-      headers: {
-        CommonApi.userAgent: userAgent,
-      }
-    ));
+    final response =
+        await dioRequest.get(searchURL.replaceFirst("{keyword}", keyword),
+            options: Options(headers: {
+              CommonApi.userAgent: userAgent,
+            }));
     logger.i(response.data);
+    VideoResourcesController.parseSearchHtml(response.data);
     return '';
   }
 }
