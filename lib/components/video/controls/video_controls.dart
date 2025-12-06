ad0d01884b643c3965e23eee17dd3821013ad1a9
LiggMax <wenzhouli06@gmail.com>
2025-12-06 08:59:55 +0800

    refactor(video): 拆分视频控件UI组件优化代码结构
    - 将顶部区域提取为独立的buildTopArea组件
    - 将底部区域提取为独立的buildBottomArea组件

--- lib/components/video/controls/video_controls.dart
+++ lib/components/video/controls/video_controls.dart
@@ -45,91 +45,9 @@ class _VideoControlsUiState extends State<VideoControlsUi> {
 
   @override
   Widget build(BuildContext context) {
-    //使用media_kit_video提供的全屏判断
-    bool fullscreen = isFullscreen(context);
     return Column(children: [
       ///顶部
-      Obx(() => AnimatedSize(
-            duration: const Duration(milliseconds: 300),
-            curve: Curves.easeInOut,
-            child: videoUiStateController.isShowControlsUi.value
-                ? Container(
-                    padding: EdgeInsets.only(
-                        top: MediaQuery.of(context).padding.top),
-                    decoration: BoxDecoration(
-                        gradient: LinearGradient(
-                            colors: [Colors.black45, Colors.transparent],
-                            begin: Alignment.topCenter,
-                            end: Alignment.bottomCenter)),
-                    child: Padding(
-                      padding: EdgeInsets.symmetric(horizontal: 3),
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          Row(children: [
-                            IconButton(
-                                onPressed: () {
-                                  Get.back();
-                                },
-                                icon: Icon(
-                                  Icons.arrow_back_ios_new,
-                                  color: Colors.white.withValues(alpha: 0.8),
-                                )),
-                            SizedBox(width: 5),
-                            if (fullscreen)
-                              Column(
-                                children: [
-                                  Text(
-                                    widget.subject.nameCN ??
-                                        widget.subject.name,
-                                    style: TextStyle(
-                                        color: Colors.white70,
-                                        fontSize: 20,
-                                        fontWeight: FontWeight.bold),
-                                  )
-                                  //后续添加剧集名称
-                                ],
-                              )
-                          ]),
-                          //右侧
-                          Row(
-                            children: [
-                              if (PlatformUtil.isDesktop)
-                                Obx(() => playPageController.isWideScreen.value
-                                    ? IconButton(
-                                        onPressed: () => playPageController
-                                            .toggleContentExpanded(),
-                                        padding: EdgeInsets.all(0),
-                                        icon: playPageController
-                                                .isContentExpanded.value
-                                            ? SvgPicture.asset(
-                                                "lib/assets/icons/right_panel_close.svg",
-                                                width: 30,
-                                                height: 30,
-                                                colorFilter: ColorFilter.mode(
-                                                  Colors.white70,
-                                                  BlendMode.srcIn,
-                                                ),
-                                              )
-                                            : SvgPicture.asset(
-                                                "lib/assets/icons/left_panel_close.svg",
-                                                width: 30,
-                                                height: 30,
-                                                colorFilter: ColorFilter.mode(
-                                                  Colors.white70,
-                                                  BlendMode.srcIn,
-                                                ),
-                                              ),
-                                      )
-                                    : SizedBox.shrink())
-                            ],
-                          )
-                        ],
-                      ),
-                    ),
-                  )
-                : SizedBox.shrink(),
-          )),
+      Obx(() => buildTopArea()),
 
       ///中间占满剩余区域
       Expanded(
@@ -140,108 +58,93 @@ class _VideoControlsUiState extends State<VideoControlsUi> {
               : ControlGestureDetector(child: buildMiddleArea())),
 
       ///底部
-      Obx(() => AnimatedSize(
-            duration: const Duration(milliseconds: 300),
-            curve: Curves.easeInOut,
-            child: videoUiStateController.isShowControlsUi.value
-                ? Container(
-                    decoration: BoxDecoration(
-                      gradient: LinearGradient(
-                          colors: [
-                            Colors.black38,
-                            Colors.transparent,
-                          ],
-                          begin: Alignment.bottomCenter,
-                          end: Alignment.topCenter),
-                    ),
-                    child: Padding(
-                      padding: EdgeInsets.all(3),
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          // 时间显示组件
-                          Padding(
-                            padding: EdgeInsets.symmetric(horizontal: 12),
-                            child: VideoTimeDisplay(
-                                videoController: videoUiStateController),
-                          ),
-
-                          Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                            crossAxisAlignment: CrossAxisAlignment.center,
-                            children: [
-                              // 播放按钮
-                              AnimatedSwitcher(
-                                duration: Duration(milliseconds: 300),
-                                transitionBuilder: (Widget child,
-                                    Animation<double> animation) {
-                                  return FadeTransition(
-                                    opacity: animation,
-                                    child: ScaleTransition(
-                                      scale: animation,
-                                      child: RotationTransition(
-                                        turns:
-                                            Tween<double>(begin: 0.5, end: 1.0)
-                                                .animate(animation),
-                                        child: child,
-                                      ),
-                                    ),
-                                  );
-                                },
-                                child: Obx(() => IconButton(
-                                    padding: EdgeInsets.all(0),
-                                    key: ValueKey<bool>(
-                                        videoUiStateController.isPlaying),
-                                    onPressed:
-                                        videoUiStateController.togglePlay,
-                                    icon: Icon(
-                                      videoUiStateController.isPlaying
-                                          ? Icons.pause_rounded
-                                          : Icons.play_arrow_rounded,
-                                      size: 33,
-                                      color:
-                                          Colors.white.withValues(alpha: 0.8),
-                                    ))),
-                              ),
-
-                              // 进度条
-                              Expanded(
-                                child: VideoProgressBar(
-                                    videoUiStateController:
-                                        videoUiStateController),
-                              ),
-
-                              // 全屏按钮
-                              AnimatedContainer(
-                                duration: Duration(milliseconds: 500),
-                                child: IconButton(
-                                  //使用media_kit_video提供的全屏方法
-                                  onPressed: () => toggleFullscreen(context),
-                                  padding: EdgeInsets.all(0),
-                                  icon: fullscreen
-                                      ? Icon(
-                                          size: 33,
-                                          Icons.fullscreen_exit,
-                                          color: Colors.white
-                                              .withValues(alpha: 0.8),
-                                        )
-                                      : Icon(
-                                          size: 33,
-                                          Icons.fullscreen,
-                                          color: Colors.white
-                                              .withValues(alpha: 0.8),
-                                        ),
-                                ),
-                              ),
-                            ],
-                          ),
-                        ],
-                      ),
-                    ),
-                  )
-                : SizedBox.shrink(),
-          ))
+      Obx(() => buildBottomArea())
     ]);
+  }
+
+  //顶部区域组件
+  Widget buildTopArea() {
+    //使用media_kit_video提供的全屏判断
+    bool fullscreen = isFullscreen(context);
+    return AnimatedSize(
+      duration: const Duration(milliseconds: 300),
+      curve: Curves.easeInOut,
+      child: videoUiStateController.isShowControlsUi.value
+          ? Container(
+              padding: EdgeInsets.only(top: MediaQuery.of(context).padding.top),
+              decoration: BoxDecoration(
+                  gradient: LinearGradient(
+                      colors: [Colors.black45, Colors.transparent],
+                      begin: Alignment.topCenter,
+                      end: Alignment.bottomCenter)),
+              child: Padding(
+                padding: EdgeInsets.symmetric(horizontal: 3),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Row(children: [
+                      IconButton(
+                          onPressed: () {
+                            Get.back();
+                          },
+                          icon: Icon(
+                            Icons.arrow_back_ios_new,
+                            color: Colors.white.withValues(alpha: 0.8),
+                          )),
+                      SizedBox(width: 5),
+                      if (fullscreen)
+                        Column(
+                          children: [
+                            Text(
+                              widget.subject.nameCN ?? widget.subject.name,
+                              style: TextStyle(
+                                  color: Colors.white70,
+                                  fontSize: 20,
+                                  fontWeight: FontWeight.bold),
+                            )
+                            //后续添加剧集名称
+                          ],
+                        )
+                    ]),
+                    //右侧
+                    Row(
+                      children: [
+                        if (PlatformUtil.isDesktop)
+                          Obx(() => playPageController.isWideScreen.value
+                              ? IconButton(
+                                  onPressed: () => playPageController
+                                      .toggleContentExpanded(),
+                                  padding: EdgeInsets.all(0),
+                                  icon:
+                                      playPageController.isContentExpanded.value
+                                          ? SvgPicture.asset(
+                                              "lib/assets/icons/right_panel_close.svg",
+                                              width: 30,
+                                              height: 30,
+                                              colorFilter: ColorFilter.mode(
+                                                Colors.white70,
+                                                BlendMode.srcIn,
+                                              ),
+                                            )
+                                          : SvgPicture.asset(
+                                              "lib/assets/icons/left_panel_close.svg",
+                                              width: 30,
+                                              height: 30,
+                                              colorFilter: ColorFilter.mode(
+                                                Colors.white70,
+                                                BlendMode.srcIn,
+                                              ),
+                                            ),
+                                )
+                              : SizedBox.shrink())
+                      ],
+                    )
+                  ],
+                ),
+              ),
+            )
+          : SizedBox.shrink(),
+    );
   }
 
   //中间区域组件
@@ -351,11 +254,108 @@ class _VideoControlsUiState extends State<VideoControlsUi> {
                   ),
                 )
               : SizedBox.shrink()),
-
           // 播放状态图标
           PlayStatusIcon(videoUiStateController),
         ],
       ),
+    );
+  }
+
+  //底部区域组件
+  Widget buildBottomArea() {
+    //使用media_kit_video提供的全屏判断
+    bool fullscreen = isFullscreen(context);
+    return AnimatedSize(
+      duration: const Duration(milliseconds: 300),
+      curve: Curves.easeInOut,
+      child: videoUiStateController.isShowControlsUi.value
+          ? Container(
+              decoration: BoxDecoration(
+                gradient: LinearGradient(colors: [
+                  Colors.black38,
+                  Colors.transparent,
+                ], begin: Alignment.bottomCenter, end: Alignment.topCenter),
+              ),
+              child: Padding(
+                padding: EdgeInsets.all(3),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    // 时间显示组件
+                    Padding(
+                      padding: EdgeInsets.symmetric(horizontal: 12),
+                      child: VideoTimeDisplay(
+                          videoController: videoUiStateController),
+                    ),
+
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        // 播放按钮
+                        AnimatedSwitcher(
+                          duration: Duration(milliseconds: 300),
+                          transitionBuilder:
+                              (Widget child, Animation<double> animation) {
+                            return FadeTransition(
+                              opacity: animation,
+                              child: ScaleTransition(
+                                scale: animation,
+                                child: RotationTransition(
+                                  turns: Tween<double>(begin: 0.5, end: 1.0)
+                                      .animate(animation),
+                                  child: child,
+                                ),
+                              ),
+                            );
+                          },
+                          child: Obx(() => IconButton(
+                              padding: EdgeInsets.all(0),
+                              key: ValueKey<bool>(
+                                  videoUiStateController.isPlaying),
+                              onPressed: videoUiStateController.togglePlay,
+                              icon: Icon(
+                                videoUiStateController.isPlaying
+                                    ? Icons.pause_rounded
+                                    : Icons.play_arrow_rounded,
+                                size: 33,
+                                color: Colors.white.withValues(alpha: 0.8),
+                              ))),
+                        ),
+
+                        // 进度条
+                        Expanded(
+                          child: VideoProgressBar(
+                              videoUiStateController: videoUiStateController),
+                        ),
+
+                        // 全屏按钮
+                        AnimatedContainer(
+                          duration: Duration(milliseconds: 500),
+                          child: IconButton(
+                            //使用media_kit_video提供的全屏方法
+                            onPressed: () => toggleFullscreen(context),
+                            padding: EdgeInsets.all(0),
+                            icon: fullscreen
+                                ? Icon(
+                                    size: 33,
+                                    Icons.fullscreen_exit,
+                                    color: Colors.white.withValues(alpha: 0.8),
+                                  )
+                                : Icon(
+                                    size: 33,
+                                    Icons.fullscreen,
+                                    color: Colors.white.withValues(alpha: 0.8),
+                                  ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+            )
+          : SizedBox.shrink(),
     );
   }
 }
